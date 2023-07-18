cd001379fbcc0c4c0792ac3c576b3402de9b95c6
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-07-17 22:04:22 -0600

    :sparkles: Add email sender (nodemailer) configuration

---
+++ helpers/emails.js
@@ -0,0 +1,30 @@
+import fs from 'node:fs'
+import nodemailer from 'nodemailer'
+import ejs from 'ejs'
+
+const transporter = nodemailer.createTransport({
+  host: process.env.EMAIL_HOST,
+  port: process.env.EMAIL_PORT,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  }
+})
+
+export const sendMail = async (options) => {
+  // Name of template for example: confirm-account.ejs
+  const file = __dirname + `/../views/emails/${options.file}.ejs`
+  // Compile the file
+  const compiled = ejs.compile(fs.readFileSync(file, 'utf-8'))
+  // Create the html
+  const html = compiled({ url: options.url })
+
+  const mailOptions = {
+    from: 'Meeti <noreply@meeti.com>',
+    to: options.user.email,
+    subject: options.subject,
+    html,
+  }
+
+  await transporter.sendMail(mailOptions)
+}
